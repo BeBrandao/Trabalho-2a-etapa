40508aefe78f8c717ebd881482e8cbc1238a3350
BeBrandao <beabs2305@gmail.com>
2024-11-29 21:01:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,43 +32,61 @@ function calcularMedia(){
 }
 
 let rodada = 1;
+let pontuacaojogo = 0;
 function craps(){
     let dado1 = document.getElementById("dado1");
     let dado2 = document.getElementById("dado2");
     let resultado = document.getElementById("resultado");
     let pontuacao = document.getElementById("pontuacao");
     let inicio = document.getElementById("inicio");
-    let pontuacaojogo = 0;
 
     const aleatorio1 = Math.floor(Math.random() * 6) + 1;
     const aleatorio2 = Math.floor(Math.random() * 6) + 1;
-    const soma = aleatorio1 + aleatorio2;
+    var soma = aleatorio1 + aleatorio2;
 
     if (rodada === 1){
+        resultado.textContent = `Resultado: `;
+        inicio.textContent = `Clique para Jogar`;
         if (soma === 7 || soma === 11){
             resultado.textContent = `Você ganhou!! Sua soma foi ${soma}`;
+            verificador = 2;
         } else if (soma === 2 || soma === 3 || soma === 12){
             resultado.textContent = `Craps! Você perdeu!! Sua soma foi ${soma}. Jogue novamente`;
+            verificador = 2;
         } else {
             resultado.textContent = `Pontuação Obtida Jogue Novamente!!`;
-            pontuacao.textContent = `Sua pontuação é ${soma}`;
             pontuacaojogo = soma;
+            pontuacao.textContent = `Sua pontuação é ${pontuacaojogo}`;
+            verificador = 1;
         }
-    }
+    };
+
     if (rodada != 1){
-        inicio.textContent = "";
-        if (soma === 7){
-            resultado.textContent = `Você perdeu!! Sua soma foi ${soma}. Recomeçe!`;
-            pontuacaojogo = 0;
-            rodada = 1;
-        } 
+        inicio.textContent = ``;
+        resultado.textContent = ``;
         if (soma === pontuacaojogo){
             resultado.textContent = `Você ganhou!! Sua soma foi ${soma}`;
+            verificador = 2;
+        } else if (soma === 7){
+            resultado.textContent = `Você perdeu!! Sua soma foi ${soma}. Recomeçe!`;
+            verificador = 2;
+        } else {
+            rodada += 1;
         }
-    }
-    console.log(rodada);
-    console.log(soma);
+    };
+
+    if (verificador === 1){
+        rodada = rodada + 1;
+        verificador = 0;
+    };
+    if (verificador === 2){
+        inicio.textContent = `Clique no botão para jogar novamente`;
+        pontuacao.textContent = ``
+        pontuacaojogo = 0;
+        rodada = 1;
+        verificador = 0;
+    };
+
     dado1.textContent = `Dado 1: ${aleatorio1}`;
     dado2.textContent = `Dado 2: ${aleatorio2}`;
-    rodada = rodada + 1;
 }
